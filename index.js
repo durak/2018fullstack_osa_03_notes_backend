1ab8c02b958e6d234ed21b23b1ce184638c5a08c
durak <itapkos@gmail.com>
2018-06-01 01:32:04 +0300

    refactoring: controller routes, middlewares

--- index.js
+++ index.js
@@ -1,149 +1,42 @@
-// const http = require('http')
 const express = require('express')
+const app = express()       // funktio palauttaa express-olion
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const Note = require('./models/note')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+
+const notesRouter = require('./controllers/notes')
 
 
-const logger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
 }
 
-const app = express()       // funktio palauttaa express-olion
-app.use(bodyParser.json())
-app.use(logger)
-app.use(cors())
-app.use(express.static('build'))
+const url = process.env.MONGODB_URI
 
+mongoose
+  .connect(url)
+  .then( () => {
+    console.log('connected to database', process.env.MONGODB_URI)
+  })
+  .catch( err => {
+    console.log(err)
+  })
+
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static('build'))
+app.use(middleware.logger)
 
 // routemäärittelyt
+app.use('/api/notes', notesRouter)
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello Worldd!</h1>')
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note
-    .findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(formatNote(note))
-      } else {
-        response.status(404).end()
-      }
-
-    })
-    .catch(error => {
-      console.log(error)
-      response.status(400).send({ error: 'malformatted id' })
-    })
-
-  /*   const id = Number(request.params.id)
-    const note = notes.find(note => {
-      return note.id === id
-    })
-
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    } */
-})
-
-app.get('/api/notes', (req, res) => {
-  // res.json(notes)
-  Note
-    .find({})
-    .then(notes => {
-      res.json(notes.map(formatNote))
-    })
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-  /*   const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-    response.status(204).end() */
-  Note
-    .findByIdAndRemove(request.params.id)
-    .then( () => {
-      response.status(204).end()
-    })
-    .catch( () => {
-      response.status(400).send({ error: 'malformatted id' })
-    })
-
-})
-
-app.put('/api/notes/:id', (request, response) => {
-  const body = request.body
-
-  const note = {
-    content: body.content,
-    important: body.important
-  }
-
-  Note
-    .findByIdAndUpdate(request.params.id, note, { new: true })
-    .then(updatedNote => {
-      response.json(formatNote(updatedNote))
-    })
-    .catch(error => {
-      console.log(error)
-      response.status(400).send({ error: 'malformatted id' })
-    })
-})
-
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
-  }
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date()
-  })
-
-  note
-    .save()
-    .then(formatNote)
-    .then(savedAndFormattedNote => {
-      response.json(savedAndFormattedNote)
-    })
-
-  /*   console.log('body', body)
-    console.log('req headers', request.headers) */
-})
-
-/* const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end(JSON.stringify(notes))
-}) */
-
-const error = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-app.use(error)
-
-
-/* const generateId = () => {
-  const maxId = notes.length > 0 ? notes.map(n => n.id).sort().reverse()[0] : 1
-  return maxId + 1
-} */
-
-const formatNote = (note) => {
-  return {
-    content: note.content,
-    date: note.date,
-    important: note.important,
-    id: note._id
-  }
-}
+app.use(middleware.error)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- models/note.js
+++ models/note.js
@@ -1,12 +1,5 @@
 const mongoose = require('mongoose')
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-const url = process.env.MONGODB_URI
-
-mongoose.connect(url)
 
 const Note = mongoose.model('Note', {
   content: String,
